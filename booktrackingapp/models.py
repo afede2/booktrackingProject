64b8349a527729f620408106103802853eccd418
afede2 <sofia@federico.ws>
2024-11-03 00:32:28 -0400

    Creating Sample Database, and printing content

--- booktrackingapp/models.py
+++ booktrackingapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    description = models.TextField()
+    pages = models.IntegerField()
+
+    def __str__(self):
+        return f"Book: '{self.name}' - '{self.author}' - '{self.description}' - {self.pages}"
+
+
+#class Author(models.Model):
+#    name = models.CharField(max_length=50)
+
+#class User(models.Model):
+#    name = models.CharField(max_length=50)
+#    email = models.EmailField()
+#    password = models.CharField(max_length=50)
+
+
+
