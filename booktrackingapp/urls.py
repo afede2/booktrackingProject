c22b2eb46fc9af96a50fc6b4292905c722c01990
afede2 <sofia@federico.ws>
2024-11-03 20:32:49 -0500

    adding to display pages
    creating more views to have the list of books with different statuses shown in different templates

--- booktrackingapp/urls.py
+++ booktrackingapp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 from . import views
-from . views import in_progress_book
+from . views import to_read_book, in_progress_book, completed_book
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('users/<int:user_id>/in-progress', in_progress_book, name='in_progress_books')
+    path('users/<int:user_id>/to-read', to_read_book, name='to_read_book'),
+    path('users/<int:user_id>/in-progress', in_progress_book, name='in_progress_books'),
+    path('users/<int:user_id>/completed', completed_book, name='completed_book')
+
+
 ]
--- booktrackingapp/views.py
+++ booktrackingapp/views.py
@@ -10,9 +10,23 @@ def index(request):
     return render(request, 'index.html', {'books': books, 'user_books': user_books, 'journal_entries': journal_entries})
 
 
+def to_read_book(request, user_id):
+    #query all books in UserBook database for specified user statues 0, 'to read'
+    to_read_books = UserBook.objects.filter(user_id=user_id, status=0)
+
+    #pass to template
+    return render(request, 'to-read.html', {'to_read_books': to_read_books})
+
 def in_progress_book(request, user_id):
     #query all books in UserBook database for specified user statues 1, 'in progress'
     in_progress_books = UserBook.objects.filter(user_id=user_id, status=1)
 
     #pass to template
-    return render(request, 'index.html', {'in_progress_books': in_progress_books})
+    return render(request, 'in-progress.html', {'in_progress_books': in_progress_books})
+
+def completed_book(request, user_id):
+    #query all books in UserBook database for specified user statues 2, 'completed'
+    completed_books = UserBook.objects.filter(user_id=user_id, status=2)
+
+    #pass to template
+    return render(request, 'completed.html', {'completed_books': completed_books})
